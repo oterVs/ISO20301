28c56974c8571ddc49bca4bcda8d6cdd323a2742
oterVs <vicentesaavedra45@gmail.com>
2021-07-05 14:43:38 -0500

    tercer commit

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,8 @@
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import {useState} from 'react'
-import "./Login.css"
+import {Link} from 'react-router-dom'
+//import "./Login.css"
+import "./Login.scss"
 import "bootstrap/dist/css/bootstrap.min.css";
 import Admin from "./Admin";
 
@@ -13,13 +15,6 @@ const Login = () => {
   const [form, setForm] = useState(initialForm);
 
 
-  let row = {
-      background: "#fff",
-      borderRadius: "10px",
-      height: "100%",
-     
-
-  };
 
   let col1 = {
     display: "flex"
@@ -27,8 +22,8 @@ const Login = () => {
 
   let im = {
       width: "80%",
-      display:"block",
-      marign: "auto"
+     
+
 
   }
 
@@ -58,15 +53,18 @@ const Login = () => {
  }
 
 
-
   return (
     <>
       <Container fluid className="Container">
-        <Row style={row}>
-          <Col xs={12} md={12} xl={6}>
+        <Row className="row">
+          <Col className="ColumnaUno"  xl={5}>
             <img style={im} src="../../images/team.svg" alt=""></img>
           </Col>
-          <Col style={col2} xs={12} md={12} xl={6}>
+          <Col style={col2}  md={12} xl={7}>
+            <div className="Enlaces">
+               <Link className="Enlace" to>Iniciar Sesión</Link>
+               <Link className="Enlace" to>Crear una Cuenta</Link> 
+            </div>
             <Form>
               <h2>Bienvenido a ISOTools</h2>
               <p>Registra tu cuenta</p>
